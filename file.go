d9269a74ba34922301a7962fe94dbe7b92ef1dee
Mike Leone <mleone896@gmail.com>
2015-07-21 18:48:00 -0400

    first commit

---
+++ file.go
@@ -0,0 +1,30 @@
+package sshmux
+
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+// read a list of hosts to run command on
+func GetHostsByFile(file string) []string {
+	var hosts []string
+	fr, _ := os.Open(file)
+	defer fr.Close()
+	r := bufio.NewReader(fr)
+	for {
+		readline, _ := r.ReadString('\n')
+		// ignore 'comment' lines
+		if !strings.Contains(readline, "#") {
+			// add host to list
+			if readline != "" {
+				hosts = append(hosts, strings.Trim(readline, "\n"))
+			}
+		}
+		// check for end of file
+		if readline == "" {
+			break
+		}
+	}
+	return hosts
+}
